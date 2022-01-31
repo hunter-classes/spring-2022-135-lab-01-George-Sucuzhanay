0cbfaacb8f9fdf8788f4c034919257fe81307139
George <georgesucuzhanay100@gmail.com>
2022-01-31 09:12:21 -0500

    comments on starter code and hellow world practice

---
+++ smaller.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << "\n";
+
+    // input two values 
+    // use an if to see which is bigger and print it out
+    
+
+    return 0;
+}
