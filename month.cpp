99f55034adf4e61c047685aa924c813d2ede1e62
George <georgesucuzhanay100@gmail.com>
2022-02-01 13:53:17 -0500

    completed month lab

--- month.cpp
+++ month.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     int year;
+    std::string calenderType = "";
     int month;
     int days;
 
@@ -11,5 +12,36 @@ int main()
 
     std::cout << "Enter month: ";
     std::cin >> month;
-    
+
+    if(year % 4 != 0){
+        calenderType = "common year";
+    }
+    else if(year % 100 != 0){
+        calenderType = "leap year";
+
+    }
+    else if(year % 400 != 0){
+        calenderType = "common year";
+    }
+    else{
+        calenderType = "leap year";
+
+    }
+
+    if (calenderType == "leap year" && month == 2){
+        days = 29;
+    }
+    else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+	||month == 10 || month ==12 and calenderType == "leap year"){
+        days = 31;
+    }
+    else if (calenderType == "common year" && month == 2){
+        days = 28;
+    }
+    else{
+        days = 30;
+    }
+
+    std::cout << days << " days in " << month << "/" << year << "\n";
+
 }
